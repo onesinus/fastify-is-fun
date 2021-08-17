4d5ebf302f0673fce5de0606bf3aefc7e2d1f9bf
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-08-18 05:56:54 +0700

    Try to deploy fastify to vercel

---
+++ serverless.js
@@ -0,0 +1,14 @@
+"use strict"
+
+import Fastify from 'fastify'
+
+const app = Fastify({
+    logger: true
+})
+
+app.register(import("server"))
+
+export default async (req, res) => {
+    await app.ready()
+    app.server.emit('request', req, res)
+}
